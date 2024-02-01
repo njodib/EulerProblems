c99d835167c104eb4d1850b3db8531ff2a493719
fournjb <naomibasterash@gmail.com>
2024-02-01 01:37:33 -0500

    3 digit numbers

--- problem4.py
+++ problem4.py
@@ -6,8 +6,8 @@ def is_palindrome(n):
     return n == n[::-1]
 
 max_palindrome = 0
-for i in range (10,100):
-    for j in  range (10,100):
+for i in range (100,1000):
+    for j in  range (100,1000):
         if is_palindrome(i*j) and max_palindrome < i*j:
             max_palindrome = i*j
 print(max_palindrome)
