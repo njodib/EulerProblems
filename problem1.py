e7b8a948d1ca19836b7a52e49c37d0029b1afeb6
fournjb <naomibasterash@gmail.com>
2024-02-01 01:27:48 -0500

    Header problems 1,2

--- problem1.py
+++ problem1.py
@@ -1,5 +1,8 @@
+# Multiples of 3 or 5
+# O(n) solution
+n = 1000
 sum = 0
-for i in range (1,1000):
+for i in range (1,n):
     if i%3==0 or i%5==0:
         sum += i
 print(sum)
--- problem2.py
+++ problem2.py
@@ -1,6 +1,8 @@
-a,b = 1,2
-sum = 0
-while a < 4000000:
+# Even fibonacci number sum
+# O(0.6n) solution
+n = 4000000
+a,b,sum = 1,2,0
+while a < n:
     if a%2==0:
         sum += a
     a,b = b,a+b
