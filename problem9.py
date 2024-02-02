874844cbaaad9bd7d58aa9338737bd1eb1f61319
fournjb <naomibasterash@gmail.com>
2024-02-02 00:54:30 -0500

    Solved problem 9

---
+++ problem9.py
@@ -0,0 +1,8 @@
+# Special Pythagorean Triplet
+# Find abc such that a^2+b^2=c^2 and a+b+c=1000 and a<b<c
+
+for a in range(1,333):
+    for b in range(a+1,1000-a):
+        c = 1000-a-b
+        if a**2 + b**2 == c**2:
+            print(a*b*c)
